7998abeedae1a14ac3c89157656a624ff30730f6
rhys.zhao <rhys.zhao@cloudwise.com>
2022-07-04 10:22:50 +0800

    Update 初始化拖拽缩放容器组件

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+/*
+ * Author  rhys.zhao
+ * Date  2022-01-28 15:18:07
+ * LastEditors  rhys.zhao
+ * LastEditTime  2022-07-04 10:02:07
+ * Description
+ */
+module.exports = {
+    "presets": ["@babel/preset-env", "@babel/preset-react"]
+}
---
+++ config/webpack.base.js
@@ -0,0 +1,44 @@
+/*
+ * Author  rhys.zhao
+ * Date  2022-01-28 15:23:47
+ * LastEditors  rhys.zhao
+ * LastEditTime  2022-06-07 10:28:00
+ * Description
+ */
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss|sass$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
